ec12ac59c5c6081f6cdf4543b8f9d8def70b50e7
Francis Bourre <francis_bourre@me.com>
2018-04-17 14:01:36 +0200

    Keep IInjectorContainer in the inheritance chain

--- src/hex/di/IInjectorContainer.hx
+++ src/hex/di/IInjectorContainer.hx
@@ -4,7 +4,6 @@ package hex.di;
  * @author Francis Bourre
  */
 #if !macro
-@:remove
 @:autoBuild( hex.di.annotation.AnnotationTransformer.readMetadata( hex.di.IInjectorContainer ) )
 #end
 interface IInjectorContainer extends IInjectorAcceptor
